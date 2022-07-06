82cc740bafd9d8095d44cce67e914b1093f72337
Nyakara96 <nyakaralauren>
2022-07-06 16:27:38 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ *  _isalpha-checks for letters
+ *  @c-registers charaxters
+ *  Return:Reurns either 0 or |
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'))
+}
