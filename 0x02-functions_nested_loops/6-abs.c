462217465cd1a2feb4e27d8333d5c8bb28f52182
Nyakara96 <nyakaralauren>
2022-07-06 17:02:38 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * _abs-prints a bsolute value of a number
+ * @n:registers an integer
+ * Reeturn:1 if above 0,0 if 0 ,and -1 if below 0
+ */
+int _abs(int);
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
