7a08a003a9091654cdc5301777969ac47027c5de
Nyakara96 <nyakaralauren>
2022-07-07 02:00:30 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,16 @@
 #include"main.h"
 /**
- * a function that prints the last digits
+ * print_last_digit-a function that prints the last digits
  * @n-takes in an integer
  * Result:returns an output
  */
 int print_last_digit(int)
 {
-	int ln = n % 10;
+	int x;
 
 	if (n < 0)
-		ln = ln* -1;
-	_putchar(ln + '0');
-	return (ln);
+	    n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (n % 10);
 }
