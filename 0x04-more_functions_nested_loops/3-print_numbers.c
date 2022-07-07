50932581948cfc99d428022eeaeb03dfa6fae760
Nyakara96 <nyakaralauren>
2022-07-07 11:55:21 -0700

    fix for task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
-#include"main.h"
+#include "main.h"
+
 /**
  * print_most_numbers -> this prints most numbers
  */
@@ -8,7 +9,7 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		if (x != 2 && x != 4)
+	    if (x != 2 && x != 4)
 		_putchar(x + '0');
 	}
 	_putchar('\n');
